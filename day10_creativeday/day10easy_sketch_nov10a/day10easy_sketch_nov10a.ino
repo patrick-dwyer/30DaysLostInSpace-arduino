5ab202402c115d6e7a66d872d0dcf7996ad0ea77
Patrick Dwyer <patrickjohanndwyer@gmail.com>
2025-11-12 21:46:52 -0700

    Creative day 10 (photoresistor, switch and RED LED). Day 11 keypad library first use.

---
+++ day10_creativeday/day10easy_sketch_nov10a/day10easy_sketch_nov10a.ino
@@ -0,0 +1,45 @@
+// RGB LED pins
+const byte RED_PIN = 13;
+const byte GREEN_PIN = 12;
+const byte BLUE_PIN = 11;
+
+// Photoresistor on analog pin
+const byte PHOTORESISTOR_PIN = A0;
+
+//display color for RBG LED settings, 0-255
+void displayColor(byte red_intensity, byte green_intensity, byte blue_intensity){
+  analogWrite(RED_PIN, red_intensity);
+  analogWrite(GREEN_PIN, green_intensity);
+  analogWrite(BLUE_PIN, blue_intensity);
+}
+
+void setup() {
+  // Declare RGB LED pins as outputs
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+
+  // Start serial monitor
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  
+  // Read analog and add to battery level
+  short light_value = analogRead(PHOTORESISTOR_PIN);
+
+  // Print photoresistor value for debugging
+  Serial.println(analogRead(PHOTORESISTOR_PIN));
+  delay(100);
+
+  // Conditional: low light, light on. High light, light off.
+  if (light_value < 30){
+    displayColor(255, 255, 255);
+  } else {
+    displayColor(0, 0, 0);
+  }
+
+
+
+}
